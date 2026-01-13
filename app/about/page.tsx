7c1491684b81740b62e56937b74be1487d172045
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2026-01-13 16:37:38 -0500

    About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,52 @@
 export default function About() {
   return (
+    <>
+      <div className="card">
+        <h2 className="subheading">Introduction</h2>
+        <p>
+          Hey, I'm Henry Ye Cao, a Computer Science graduate.
+        </p>
+      </div>
 
+      <div className="card">
+        <h2 className="subheading">Skills</h2>
+
+        <button className="skill-button">Python</button>
+        <button className="skill-button">Java</button>
+        <button className="skill-button">JavaScript</button>
+        <button className="skill-button">C</button>
+        <button className="skill-button">HTML/CSS</button>
+        <button className="skill-button">MySQL</button>
+        <button className="skill-button">OOP</button>
+        <button className="skill-button">DSA</button>
+        <button className="skill-button">Git</button>
+      </div>
+
+      <div className="card">
+        <h2 className="subheading">Volunteer Experiences</h2>
+
+        <h3 className="job-title">Web Developer</h3>
+        <div className="job-content">
+          White Haven Community Club - Scarborough, ON
+          <br />
+          Oct 2025 to Now
+          <br />
+          Some of the tasks I performed:
+          <ul className="list-disc pl-5">
+            <li>
+              Improve backend API endpoint (error checking and validation) and database connection
+              (connection pool instead of standard connection).
+            </li>
+            <li>
+              Implement Google reCAPTCHA for form submission to prevent spam.
+            </li>
+            <li>
+              Improve backend security to prevent attacks such as SQL injection with parameter placeholder,
+              “?”, XSS with sanitized inputs, and CSRF using CORS.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
