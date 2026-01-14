77589341a16452a880e0e295a71fcff6b2755691
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2026-01-13 21:46:41 -0500

    Contact

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div className="card">
+      <p>
+        Interested in collaborating?
+        <br />
+        Send me an email at <a className="font-bold text-blue-500 underline" href="mailto:hyecao11@protonmail.com">hyecao11@protonmail.com</a>
+      </p>
+    </div>
+  );
+}
